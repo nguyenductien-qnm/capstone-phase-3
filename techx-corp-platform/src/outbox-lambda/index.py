08a24660e39c0120e20feacd07c7bc5d5cffb550
feniks <zuginphan@gmail.com>
2026-07-20 03:09:49 +0000

    feat(lambda): add outbox lambda code to push messages from dynamoStream to Kafka

---
+++ techx-corp-platform/src/outbox-lambda/index.py
@@ -0,0 +1,66 @@
+import os
+import json
+import boto3
+from kafka import KafkaProducer
+
+# Define in a global scope (warm start)
+secrets_client = boto3.client('secretsmanager')
+
+MSK_CREDENTIALS_SECRET_ARN = os.environ['MSK_CREDENTAILS_SECRET_ARN']
+MSK_ENDPOINT_SECRET_ARN = os.environ['MSK_ENDPOINT_SECRET_ARN']
+KAFKA_TOPIC = os.environ.get('KAFKA_TOPIC', 'orders')
+
+# Get secret from AWS Secrets Manager 
+def get_secret(secret_arn):
+	response = secrets_client.get_secret_value(secret_arn)
+	return json.loads(response['SecretString'])
+
+# Initialize Kafka Producer
+def init_producer():
+	global producer 
+	if producer is not None:
+		return 
+	
+	# Get MSK credentials & endpoint 
+	msk_credentials = get_secret(MSK_CREDENTIALS_SECRET_ARN)
+	msk_endpoint = get_secret(MSK_ENDPOINT_SECRET_ARN)
+	
+	brokers = msk_endpoint['broker_sasl_scram'].split(',')
+	
+	producer = KafkaProducer(
+		bootstrap_servers=brokers,
+		security_protocal='SASL_SSL',
+		sasl_mechanism='SCRAM-SHA-512',
+		sasl_plan_username=msk_credentials['username'],
+
+		key_serializer=lambda k: k.encode('utf-8'),
+		value_serializer=lambda v: v.encode('utf-8')
+	)
+	
+def handler(event, context):
+	"""
+	Purpose: Main Entrypoint 
+	"""
+	init_producer()
+ 
+	# Batch processing
+	for record in event.get('Records', []):
+		if record['eventName'] in ['INSERT', 'MODIFY']:
+			new_image = record['dynamodb']['NewImage']
+   
+			order_id = new_image.get('order_id', {}).get('S')
+			order_data_str = new_image.get('order_data', {}).get('S')
+   
+			if not order_data_str:
+				print(f"Skipping order {order_id}: No 'order_data' found in DB record.")
+				continue
+   
+			producer.send(KAFKA_TOPIC, key=order_id, value=order_data_str)
+			print(f"Successfully relayed order {order_id} to MSK topic: {KAFKA_TOPIC}")
+		
+	producer.flush()
+ 
+	return {
+		'statusCode': 200,
+		'body': json.dumps('Messages processed successfully')
+	}
