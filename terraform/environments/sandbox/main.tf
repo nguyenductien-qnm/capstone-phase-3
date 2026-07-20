16d46aa0a56c26f5d78030199e3c85359d53f81a
hofang <ringhost42@gmail.com>
2026-07-20 11:02:41 +0700

    feat(infra/terraform): enable rds logical replication in prod

--- terraform/environments/sandbox/main.tf
+++ terraform/environments/sandbox/main.tf
@@ -92,6 +92,7 @@ module "rds" {
   enable_rds_proxy           = var.enable_rds_proxy
   multi_az                   = var.rds_multi_az
   eks_node_security_group_id = module.eks.cluster_security_group_id
+  enable_logical_replication = true
 }
 
 module "elasticache" {
