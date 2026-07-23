5815b75baf40aa34f14ac241ce070aae68a0a9a4
feniks <zuginphan@gmail.com>
2026-07-23 18:39:18 +0000

    fix(infra): format kafka_topics.tf

--- terraform/environments/develop/kafka_topics.tf
+++ terraform/environments/develop/kafka_topics.tf
@@ -3,13 +3,13 @@ data "aws_secretsmanager_secret_version" "msk_credentials" {
 }
 
 resource "kafka_topic" "checkout_orders" {
-	name = "domain.checkout.orders"
-	replication_factor = 2
-	partitions = 3 
+  name               = "domain.checkout.orders"
+  replication_factor = 2
+  partitions         = 3
 }
 
 resource "kafka_topic" "fulfillment_events" {
-	name = "domain.fulfillment.events"
-	replication_factor = 2 
-	partitions = 3
+  name               = "domain.fulfillment.events"
+  replication_factor = 2
+  partitions         = 3
 }
